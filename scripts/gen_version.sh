acba077402ebeedbeb4c8fde813722e02e577b23
Colby Haskell <cchaskel@usc.edu>
2022-08-27 00:01:03 -0700

    Fix: Remove Echos From Gen Version Script

--- scripts/gen_version.sh
+++ scripts/gen_version.sh
@@ -3,10 +3,6 @@
 git=$(sh /etc/profile; which git)
 number_of_commits=$(git log --oneline $(git describe --tags --abbrev=0).. | wc -l)
 git_release_version=$("$git" describe --tags --always --abbrev=0)
-myString="${myString:1}"
-
-echo "$number_of_commits"
-echo "$git_release_version"
 
 target_plist="$TARGET_BUILD_DIR/$INFOPLIST_PATH"
 dsym_plist="$DWARF_DSYM_FOLDER_PATH/$DWARF_DSYM_FILE_NAME/Contents/Info.plist"
